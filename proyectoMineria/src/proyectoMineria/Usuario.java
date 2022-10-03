35bafdd10389a26002b82b92fea5fa3b775880a9
Tavo-94 <gustavo.mph@gmail.com>
2022-10-03 17:52:15 -0300

    getters y setters en la clase usuario

--- proyectoMineria/src/proyectoMineria/Usuario.java
+++ proyectoMineria/src/proyectoMineria/Usuario.java
@@ -3,7 +3,7 @@ package proyectoMineria;
 public abstract class Usuario {
 	private String nombre;
 	private String apellido;
-	private String nombreUsuario;
+	private String nombreUsuario; //Clave primaria de la clase
 	private String clave;
 	private String cargo;
 	private Boolean estadoActivo;
@@ -15,4 +15,44 @@ public abstract class Usuario {
 	public abstract void loguearse(String nombreDeUsuario, String clave);
 	public abstract void cambiarClave(String claveNueva);
 	public abstract void cerrarSesion();
+	
+	//getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public Boolean getEstadoActivo() {
+		return estadoActivo;
+	}
+	public void setEstadoActivo(Boolean estadoActivo) {
+		this.estadoActivo = estadoActivo;
+	}
+	
+	
 }
