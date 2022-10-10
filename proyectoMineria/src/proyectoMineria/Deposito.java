8143fa922759536b9e565be2d87de663ff989da7
EnriqueArielLopez <ariellopez2097@gmail.com>
2022-10-10 16:08:12 -0300

    stock
    Agregar materiales y muestra de stock en el deposito

--- proyectoMineria/src/proyectoMineria/Deposito.java
+++ proyectoMineria/src/proyectoMineria/Deposito.java
@@ -1,8 +1,21 @@
 package proyectoMineria;
 import java.util.*;
 
+import Dominio.Clientes;
+
 public class Deposito {
-ArrayList<Material> stock = new ArrayList <Material> ();
-	stock.add(new Material("Oro",23,23)); //ejemplo
+
+	private List <Material> stock;
+		
 	
+	public void agregarMaterial(String nombre,Double pureza,Double cantidad) {
+	stock.add(Material);
+	
+	}
+	
+	public void mostrarStock() {
+		for (Material s : stock) {
+			System.out.println(s.getNombre()+" "+s.getPureza()+" "+ s.getCantidad()"\n");
+		}
+	}
 }
