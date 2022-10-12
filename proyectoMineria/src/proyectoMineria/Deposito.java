f6d481c88c871c46c2515776a65c60ae50b2229b
EnriqueArielLopez <ariellopez2097@gmail.com>
2022-10-11 21:16:56 -0300

    DEPOSITO
    metodo mostrar lista deposito

--- proyectoMineria/src/proyectoMineria/Deposito.java
+++ proyectoMineria/src/proyectoMineria/Deposito.java
@@ -1,21 +1,102 @@
 package proyectoMineria;
-import java.util.*;
 
-import Dominio.Clientes;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Deposito {
+    
+    private List<Material> listaMateriales;
+    private Double totalDeOro = 0d;
+    private Double totalDePlata = 0d;
+    private Double totalDeCobre = 0d;
 
-	private List <Material> stock;
-		
-	
-	public void agregarMaterial(String nombre,Double pureza,Double cantidad) {
-	stock.add(Material);
-	
-	}
-	
-	public void mostrarStock() {
-		for (Material s : stock) {
-			System.out.println(s.getNombre()+" "+s.getPureza()+" "+ s.getCantidad()"\n");
-		}
-	}
+    
+    public Deposito() {
+        super();
+        this.listaMateriales = new ArrayList<>();
+    }
+    
+    public void mostrarStock() {
+        listaMateriales.forEach(System.out::println);
+    }
+    
+    public void agregarStock(Usuario usuario, String tipo, Double pureza, Double cantidad) {
+        if (this.validarCargo(usuario)) {
+            listaMateriales.add(new Material(tipo, pureza, cantidad));
+            System.out.println("Se Agrego Exitosamente!!!");
+            if (tipo.equalsIgnoreCase("oro")) {
+                this.totalDeOro += cantidad;
+            }
+            if (tipo.equalsIgnoreCase("plata")) {
+                this.totalDePlata += cantidad;
+            }
+            if (tipo.equalsIgnoreCase("cobre")) {
+                this.totalDeCobre += cantidad;
+            }
+        }
+    }
+
+    public void visualizarStock() {
+        
+        for(int i=0;i<listaMateriales.size();i++) {
+        	System.out.println(listaMateriales.get(i));
+        }
+    }
+    private boolean validarCargo(Usuario usuario) {
+        // TODO Auto-generated method stub
+        return usuario.getCargo().equalsIgnoreCase("Stock");
+    }
+
+
+    public Double sumaTotalDeOro() {
+        return this.listaMateriales.stream().filter(m -> m.getTipo().equalsIgnoreCase("oro")).mapToDouble(m -> m.getCantidad()).sum() ;
+        
+    }
+
+    public Double sumaTotalDePlata() {
+        // TODO Auto-generated method stub
+        return this.listaMateriales.stream().filter(m -> m.getTipo().equalsIgnoreCase("plata")).mapToDouble(m -> m.getCantidad()).sum() ;
+    }
+
+    public Double sumaTotalDeCobre() {
+        // TODO Auto-generated method stub
+        return this.listaMateriales.stream().filter(m -> m.getTipo().equalsIgnoreCase("cobre")).mapToDouble(m -> m.getCantidad()).sum() ;
+    }
+    
+    //getters y setters
+    
+    public List<Material> getListaMateriales() {
+        return listaMateriales;
+    }
+    
+    public void setListaMateriales(List<Material> listaMateriales) {
+        this.listaMateriales = listaMateriales;
+    }
+
+    public Double getTotalDeOro() {
+        return totalDeOro;
+    }
+
+    public void setTotalDeOro(Double totalDeOro) {
+        this.totalDeOro = totalDeOro;
+    }
+
+    public Double getTotalDePlata() {
+        return totalDePlata;
+    }
+
+    public void setTotalDePlata(Double totalDePlata) {
+        this.totalDePlata = totalDePlata;
+    }
+
+    public Double getTotalDeCobre() {
+        return totalDeCobre;
+    }
+
+    public void setTotalDeCobre(Double totalDeCobre) {
+        this.totalDeCobre = totalDeCobre;
+    }
+    
 }
