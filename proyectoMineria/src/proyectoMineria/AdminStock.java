d366bef668a69ca11137ece449631bf00dfb15e7
Denise-juli <112222707+Denise-juli@users.noreply.github.com>
2022-10-23 18:16:03 -0300

    Update AdminStock.java

--- proyectoMineria/src/proyectoMineria/AdminStock.java
+++ proyectoMineria/src/proyectoMineria/AdminStock.java
@@ -1,7 +1,12 @@
 package proyectoMineria;
+
 import java.util.Scanner;
 
-public class AdminStock {
+public class AdminStock extends Usuario {
+	
+    public AdminStock(String nombreUsuario, String clave, String cargo, Boolean estadoActivo, Mineria mineria) {
+        super(nombreUsuario, clave, cargo, estadoActivo, mineria);
+    }
 	
 	static Scanner entrada = new Scanner(System.in);
 	
@@ -19,7 +24,6 @@ public class AdminStock {
 		material.setTipo(null);
 		material.setCantidad(null);
 		material.setPureza(null);
-		material.setPrecio(null);
 		material.setFecha(null);
 		stock.add(material);
 	}
@@ -30,7 +34,8 @@ public class AdminStock {
 		System.out.println("1.Agregar material ");
 		System.out.println("2.Buscar material");
 		System.out.println("3.Modificar Stock");
-		System.out.println("4.Salir");
+		System.out.println("3.Mostrar Stock");
+		System.out.println("5.Salir");
 		
 		int opcion = entrada.nextInt();
 		switch (opcion) {
@@ -40,7 +45,9 @@ public class AdminStock {
 			break;
 		case 3: modificarStock(null);
 			break;
-		case 4: Salir();
+		case 4: mostrarStock();
+		    break;
+		case 5: Salir();
 			break;
 			default: System.out.println("Se eligio una opci�n incorrecta volver a intentar");
 				break;
@@ -55,16 +62,18 @@ public class AdminStock {
 		material.setCantidad(entrada.nextDouble());
         System.out.println("ingresar pureza");
         material.setPureza(entrada.nextDouble());
-       		if(stock.add(material)== true) {
-				 System.out.println("Se agrego correctamente el alumno");
-			 } else {
-				 System.out.println("no se pudo agregar correctamente");
-					System.out.println("si deseas agregar nuevamente un material ");
-					System.out.println("seleccionar opcion 1");	
-					}
+        System.out.println("ingresar fecha");
+        material.setFecha(entrada.nextDouble());
+        if(stock.add(material)== true) {
+        	System.out.println("Se agrego correctamente el alumno");
+        } else {
+        	System.out.println("no se pudo agregar correctamente");
+        	System.out.println("si deseas agregar nuevamente un material ");
+			System.out.println("seleccionar opcion 1");
+			}
        		imprimirMenu();
-       		}
-	
+	}
+       		
 	private void buscarMaterial() {
 		System.out.println("Ingrese tipo de material que desea buscar");
 		String tipo = entrada.next();
@@ -94,28 +103,56 @@ public class AdminStock {
 		
 		 imprimirMenu();
 	}
-	
+	private void mostrarStock() {
+		System.out.println(stock);
+		imprimirMenu();
+		
+	}
 	private void Salir() {
 		System.out.println("El programa finalizo");
 		System.exit(0);
 	}
+    @Override
+    public String toString() {
+        return "AdminStock [getNombre()=" + getNombre() + ", getApellido()=" + getApellido() + ", getNombreUsuario()="
+                + getNombreUsuario() + ", getClave()=" + getClave() + ", getCargo()=" + getCargo()
+                + ", getEstadoActivo()=" + getEstadoActivo() + ", getSessionActiva()=" + getSessionActiva()
+                + ", getMineria()=" + getMineria() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+                + ", toString()=" + super.toString() + "]";
+    }
 
-	/*private void menuAlter(Deposito material) {
-		System.out.println("1.Oro");
-		System.out.println("2.Plata");
-		System.out.println("3.Cobre");
+	public static Scanner getEntrada() {
+		return entrada;
+	}
+
+	public static void setEntrada(Scanner entrada) {
+		AdminStock.entrada = entrada;
+	}
+
+	public static Deposito getStock() {
+		return stock;
+	}
+
+	public static void setStock(Deposito stock) {
+		AdminStock.stock = stock;
+	}
+
+	@Override
+	public void loguearse(Scanner inputDelUsuario) {
+		// TODO Auto-generated method stub
 		
-		int opcion = entrada.nextInt();
-		switch (opcion) {
-		case 1: Oro(material);
-			break;
-		case 2: Plata(material);
-			break;
-		case 3: Cobre(material);
-			break;
-			default: System.out.println("Se eligio una opci�n incorrecta volver a intentar");
-				break;
-			}
-	}*/
+	}
+
+	@Override
+	public void cambiarClave(Scanner inputUsuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cerrarSesion() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
