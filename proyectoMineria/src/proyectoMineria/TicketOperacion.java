1c33eadd577ec1c00d3866ea9b78c139c93016da
Denise-juli <112222707+Denise-juli@users.noreply.github.com>
2022-10-27 21:07:12 -0300

    Update TicketOperacion.java

--- proyectoMineria/src/proyectoMineria/TicketOperacion.java
+++ proyectoMineria/src/proyectoMineria/TicketOperacion.java
@@ -1,15 +1,14 @@
 package proyectoMineria;
+
 import java.time.LocalDate;
 import java.util.LinkedList;
 
 public class TicketOperacion {
 
 	LinkedList<TicketOperacion> Venta = new LinkedList<TicketOperacion>();
-	LinkedList<Cliente> Base = new LinkedList<Cliente>();
 	
     private Double cantidadDeMaterialComprado = 0.0;
     private Material material;
-    private double total; //descuento
     private Double precio;
     private Cliente cliente;
     private LocalDate fechaDeOperacion;
@@ -19,7 +18,7 @@ public class TicketOperacion {
     
     
     
-    public TicketOperacion(Double cantidadDeMaterialComprado, Cliente cliente, AdminVentas vendedor, Double precio, Material Material) {
+    public TicketOperacion(Material Material, Double costo, Double cantidadDeMaterialComprado, Double precio, Cliente cliente, AdminVentas vendedor) {
         super();
         this.cantidadDeMaterialComprado = cantidadDeMaterialComprado;
         this.material = Material;
@@ -29,60 +28,21 @@ public class TicketOperacion {
         this.vendedor = vendedor;
     }
     
-    public void add(TicketOperacion ticket) {
-    	cliente.getDni();
-    	ticket.getCantidadDeMaterialComprado();
-	if(ticket.getCantidadDeMaterialComprado()!=0) {
-    	material.getTipo();
-    	if (material.tipo.equals("oro"));{
-        	if (material.getPureza()<=100 && material.getPureza()>=70) {
-        		double costo = ticket.getCantidadDeMaterialComprado() * 100000;
-        		ticket.setPrecio (costo);
-        		}
-        		if (material.getPureza()<=69 && material.getPureza()>=40) {
-       			double costo = ticket.getCantidadDeMaterialComprado() * 75000;
-       			ticket.setPrecio(costo);
-       			}
-       			if (material.getPureza()<=49 && material.getPureza()>=10) {
-       			double costo = ticket.getCantidadDeMaterialComprado() * 5000;
-       			ticket.setPrecio(costo);
-       			}
-       		}
-		if (material.tipo.equals("cobre"));{
-        	if (material.getPureza()<=100 && material.getPureza()>=70) {
-        		double costo = ticket.getCantidadDeMaterialComprado() * 1000;
-        		ticket.setPrecio(costo);
-        		}
-        		if (material.getPureza()<=69 && material.getPureza()>=40) {
-       			double costo = ticket.getCantidadDeMaterialComprado() * 750;
-       			ticket.setPrecio(costo);
-       			}
-       			if (material.getPureza()<=49 && material.getPureza()>=10) {
-       			double costo = ticket.getCantidadDeMaterialComprado() * 500;
-       			ticket.setPrecio(costo);
-       			}
-       		}
-		if (cliente.getEsMayorista()== true) {
-			double Ctotal = 0.8 * ticket.getPrecio();
-			ticket.setTotal(Ctotal);
-			}
-		ticket.setTotal(ticket.getPrecio());
-		}
-    }
-
 	public TicketOperacion(LinkedList<TicketOperacion> venta, LinkedList<Cliente> base,
-			Double cantidadDeMaterialComprado, Material material, double total, Double precio, Cliente cliente,
-			LocalDate fechaDeOperacion, AdminVentas vendedor) {
+			Double cantidadDeMaterialComprado, Material material, Double precio, Cliente cliente,
+			LocalDate fechaDeOperacion, AdminVentas vendedor, Double costo) {
 		super();
 		Venta = venta;
-		Base = base;
 		this.cantidadDeMaterialComprado = cantidadDeMaterialComprado;
 		this.material = material;
-		this.total = total;
 		this.precio = precio;
 		this.cliente = cliente;
 		this.fechaDeOperacion = fechaDeOperacion;
 		this.vendedor = vendedor;
+	}
+
+	public TicketOperacion() {
+		// TODO Auto-generated constructor stub
 	}
 
 	public LinkedList<TicketOperacion> getVenta() {
@@ -91,14 +51,6 @@ public class TicketOperacion {
 
 	public void setVenta(LinkedList<TicketOperacion> venta) {
 		Venta = venta;
-	}
-
-	public LinkedList<Cliente> getBase() {
-		return Base;
-	}
-
-	public void setBase(LinkedList<Cliente> base) {
-		Base = base;
 	}
 
 	public Double getCantidadDeMaterialComprado() {
@@ -115,14 +67,6 @@ public class TicketOperacion {
 
 	public void setMaterial(Material material) {
 		this.material = material;
-	}
-
-	public double getTotal() {
-		return total;
-	}
-
-	public void setTotal(double total) {
-		this.total = total;
 	}
 
 	public Double getPrecio() {
@@ -159,8 +103,9 @@ public class TicketOperacion {
 
 	@Override
 	public String toString() {
-		return "TicketOperacion [Venta=" + Venta + ", Base=" + Base + ", cantidadDeMaterialComprado="
-				+ cantidadDeMaterialComprado + ", material=" + material + ", total=" + total + ", precio=" + precio
+		return "TicketOperacion [Venta=" + Venta + ", cantidadDeMaterialComprado="
+				+ cantidadDeMaterialComprado + ", material=" + material + ", precio=" + precio
 				+ ", cliente=" + cliente + ", fechaDeOperacion=" + fechaDeOperacion + ", vendedor=" + vendedor + "]";
 	}
+
 }
