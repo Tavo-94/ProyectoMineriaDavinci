39510c086145d917a2862e01b0b54fe6d4e0b4b6
nobuwobu <carolina97ho@gmail.com>
2022-10-31 02:16:06 -0300

    probando otro codigo para interfaz

--- proyectoMineria/src/IU/InterfazGeneral.java
+++ proyectoMineria/src/IU/InterfazGeneral.java
@@ -1,37 +1,82 @@
 package IU;
 
 import javax.swing.*;
+import static javax.swing.JOptionPane.*;
 
 public class InterfazGeneral {
 
     public static void MenuPrincipal() {
 
-        int opcion = Integer
-                .parseInt(JOptionPane.showInputDialog(null, "Qué desea realizar ?\n " + "1-Mostrar lista productos\n"
-                        + "2-Agregar un producto\n" + "3-Modificar un producto\n" + "4-Eliminar un producto\n"));
+        int menuPrincipal;
 
-        switch (opcion) {
+        menuPrincipal = Integer.parseInt(showInputDialog(
+                "Bienvenido al menu principal. Por favor seleccione una opción para continuar: \n1) Signup \n 2) Login.\n3) Productos. \n4) Clientes."));
+
+        if (menuPrincipal == 1) {
+            // SIGNUP //
+            ImageIcon signup = new ImageIcon("/img/contrasena.png");
+            showMessageDialog(null,
+                    "",
+                    "SIGNUP/LOGIN", WARNING_MESSAGE, signup);
+        } else if (menuPrincipal == 2) {
+            // LOGIN //
+            ImageIcon login = new ImageIcon("/img/contrasena.png");
+            showMessageDialog(null,
+                    "",
+                    "SIGNUP/LOGIN", WARNING_MESSAGE, login);
+        } else if (menuPrincipal == 3) {
+            // PRODUCTOS //
+            ImageIcon producto = new ImageIcon("/img/producto.png");
+            showMessageDialog(null,
+                    "",
+                    "SIGNUP/LOGIN", WARNING_MESSAGE, producto);
+        } else if (menuPrincipal == 4) {
+            // CLIENTES //
+            ImageIcon cliente = new ImageIcon("/img/usuario.png");
+            showMessageDialog(null,
+                    "",
+                    "SIGNUP/LOGIN", WARNING_MESSAGE, cliente);
+
+        } else {
+            showMessageDialog(null, "Error en el ingreso!", "Aviso", JOptionPane.WARNING_MESSAGE);
+        }
+        switch (menuPrincipal) {
             case 1:
-                MostrarProductos();
+                SignUp();
                 break;
 
             case 2:
-                AgregarProducto();
+                Login();
                 break;
 
             case 3:
-                ModificarProducto();
+                Producto();
                 break;
 
             case 4:
-                EliminarProducto();
+                Cliente();
                 break;
             default:
                 JOptionPane.showMessageDialog(null, "Opcion incorrecta, vuelva a preguntar");
                 MenuPrincipal();
                 break;
-
         }
+    }
 
+    public static void SignUp() {
 
     }
+
+    public static void Login() {
+
+    }
+
+    public static void Producto() {
+
+    }
+
+    public static void Cliente() {
+
+    }
+
+}
