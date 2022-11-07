1c158c8280424cf3a6e3acf62130027c308ab135
Denise-juli <112222707+Denise-juli@users.noreply.github.com>
2022-11-07 17:50:17 -0300

    ticket

--- proyectoMineria/src/proyectoMineria/TicketOperacion.java
+++ proyectoMineria/src/proyectoMineria/TicketOperacion.java
@@ -1,106 +1,58 @@
 package proyectoMineria;
 
 
-import java.io.PrintStream;
 import java.time.LocalDate;
 
 public class TicketOperacion {
 
-    private Double cantidadDeMaterialComprado = 0.0;
-    
-    private double valorPromo = 0.0; //descuento
-    private Double montoAbonado = 0.0;
-    private Cliente cliente;
+	private Cliente cliente;
     private LocalDate fechaDeOperacion;
     private AdminVentas vendedor;
-
+    private Material materialComprado;
+    private Double total;
     
-    
-    
-    
-    public TicketOperacion(Double cantidadDeMaterialComprado, Double montoAbonado, Cliente cliente, AdminVentas vendedor) {
-        super();
-        this.cantidadDeMaterialComprado = cantidadDeMaterialComprado;
-        this.montoAbonado = montoAbonado;
-        this.cliente = cliente;
-        this.fechaDeOperacion = LocalDate.now();
-        this.vendedor = vendedor;
-    }
-
-    //excepcion si los parametros son ilegales
-    public void Alerta(double cantidadDeMaterialComprado, double valor, double valorPromo, double montoAbonado) {
-        if(cantidadDeMaterialComprado <= 0.0) {
-            throw new IllegalArgumentException("Cantidad de material tiene que ser un numero positivo.");
-        }
-        if(valor <= 0.0){
-            throw new IllegalArgumentException("Valor tiene que ser un numero positivo.");
-        }
-        if(valorPromo <= 0.0){
-            throw new IllegalArgumentException("Valor tiene que ser un numero positivo.");
-        }
-        if(montoAbonado <= 0.0) {
-            throw new IllegalArgumentException("Valor tiene que ser un numero positivo.");
-        }
-    }
-
-    //getters y setters CantidadDeMaterialComprado
-    public double getCantidadDeMaterialComprado() {
-        return cantidadDeMaterialComprado;
-    }
-    public void setCantidadDeMaterialComprado(double cantidadDeMaterialComprado) {
-        this.cantidadDeMaterialComprado = cantidadDeMaterialComprado;
-    }
-
-
-
-    //getters y setters ValorPromo
-    public double getValorPromo() {
-        return valorPromo;
-    }
-    public void setValorPromo(double valorPromo) {
-        this.valorPromo = valorPromo;
-    }
-
-    //getters y setters MontoAbonado
-    public double getMontoAbonado() {
-        return montoAbonado;
-    }
-    public void setMontoAbonado(double montoAbonado) {
-        this.montoAbonado = montoAbonado;
-    }
-
-
-    public double materialPromo(String fecha,int n) {
-        valorPromo+=getValorPromo()*n;
-        System.out.println(fecha+"\t"+"P\t"+n+"\t"+getValorPromo()+"\t"+(getValorPromo()*n)+"\n");
-        return getValorPromo()*n;
-    }
-
-    //imprime en consola el resumen de ventas
-    public void resumen() {
-        PrintStream p = System.out;
-        p.println("\t\t\tMINERIA\n");
-        p.println("Fecha\t\tTipo\tCant.\tValor\tSubtotal");
-        p.println("\t\tTotal Material comprado:\t"+totalMaterial());
-        p.println("\t\tTotal Material Promo :\t"+totalMaterialPromo());
-        p.println("\t\tTotal General :\t"+total());
-    }
-    public double total() {
-        return totalMaterial()+totalMaterialPromo();
-    }
-    public double totalMaterial() {
-        return cantidadDeMaterialComprado;
-    }
-    public double totalMaterialPromo() {
-        return valorPromo;
-    }
-
-    @Override
-    public String toString() {
-        return "TicketOperacion [cantidadDeMaterialComprado=" + cantidadDeMaterialComprado + ", valorPromo="
-                + valorPromo + ", montoAbonado=" + montoAbonado + ", cliente=" + cliente + ", fechaDeOperacion="
-                + fechaDeOperacion + ", vendedor=" + vendedor + "]";
-    }
-    
-    
+	public TicketOperacion(Cliente cliente, LocalDate fechaDeOperacion, AdminVentas vendedor, Material materialComprado,
+			Double total) {
+		super();
+		this.cliente = cliente;
+		this.fechaDeOperacion = fechaDeOperacion;
+		this.vendedor = vendedor;
+		this.materialComprado = materialComprado;
+		this.total = total;
+	}
+	@Override
+	public String toString() {
+		return "TicketOperacion [cliente=" + cliente + ", fechaDeOperacion=" + fechaDeOperacion + ", vendedor="
+				+ vendedor + ", materialComprado=" + materialComprado + ", total=" + total + "]";
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public LocalDate getFechaDeOperacion() {
+		return fechaDeOperacion;
+	}
+	public void setFechaDeOperacion(LocalDate fechaDeOperacion) {
+		this.fechaDeOperacion = fechaDeOperacion;
+	}
+	public AdminVentas getVendedor() {
+		return vendedor;
+	}
+	public void setVendedor(AdminVentas vendedor) {
+		this.vendedor = vendedor;
+	}
+	public Material getMaterialComprado() {
+		return materialComprado;
+	}
+	public void setMaterialComprado(Material materialComprado) {
+		this.materialComprado = materialComprado;
+	}
+	public Double getTotal() {
+		return total;
+	}
+	public void setTotal(Double total) {
+		this.total = total;
+	}
 }
