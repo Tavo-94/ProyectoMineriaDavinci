21eb36d30afbe959e43a66fe42c863b57f4b39a1
nobuwobu <carolina97ho@gmail.com>
2022-11-08 19:03:49 -0300

    Update Login.java

--- proyectoMineria/src/interfazWindowBuilder/Login.java
+++ proyectoMineria/src/interfazWindowBuilder/Login.java
@@ -13,15 +13,21 @@ import java.awt.event.MouseEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPasswordField;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import java.awt.Component;
+import javax.swing.SwingConstants;
 
 public class Login {
 
     private JFrame frmMetalplus;
     private JPasswordField passTxt;
     private JTextField userTxt;
+    private JFrame frmAdminsistemas;
 
     /**
      * Launch the application.
@@ -57,15 +63,54 @@ public class Login {
         frmMetalplus.getContentPane().setBackground(Color.WHITE);
         frmMetalplus.getContentPane().setLayout(null);
         
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBorder(null);
+        menuBar.setBackground(new Color(153, 204, 204));
+        menuBar.setBounds(0, 0, 709, 23);
+        frmMetalplus.getContentPane().add(menuBar);
+        
+        JMenu mnNewMenu = new JMenu("HOME");
+        mnNewMenu.setFont(new Font("JetBrains Mono NL", Font.PLAIN, 12));
+        menuBar.add(mnNewMenu);
+        
+        JMenuItem mntmNewMenuItem_8 = new JMenuItem("Menu Principal");
+        mnNewMenu.add(mntmNewMenuItem_8);
+        
+        JSeparator separator_4 = new JSeparator();
+        mnNewMenu.add(separator_4);
+        
+        JMenuItem mntmNewMenuItem_2 = new JMenuItem("AdminSistema");
+        mnNewMenu.add(mntmNewMenuItem_2);
+        
+        JMenuItem mntmNewMenuItem_3 = new JMenuItem("AdminVentas");
+        mnNewMenu.add(mntmNewMenuItem_3);
+        
+        JMenuItem mntmNewMenuItem_4 = new JMenuItem("AdminStock");
+        mnNewMenu.add(mntmNewMenuItem_4);
+        
+        JSeparator separator_1 = new JSeparator();
+        mnNewMenu.add(separator_1);
+        
+        JMenuItem mntmNewMenuItem = new JMenuItem("Login");
+        mnNewMenu.add(mntmNewMenuItem);
+        
+        JMenuItem mntmNewMenuItem_5 = new JMenuItem("Log Out");
+        mnNewMenu.add(mntmNewMenuItem_5);
+        
+        JSeparator separator_2 = new JSeparator();
+        mnNewMenu.add(separator_2);
+        
+        JMenuItem mntmNewMenuItem_1 = new JMenuItem("Close");
+        mnNewMenu.add(mntmNewMenuItem_1);
+        
+        
         JSeparator separator_1_1 = new JSeparator();
         separator_1_1.setOpaque(true);
         separator_1_1.setIgnoreRepaint(true);
         separator_1_1.setBounds(361, 295, 306, 2);
         frmMetalplus.getContentPane().add(separator_1_1);
         
-        JSeparator separator_1 = new JSeparator();
-        separator_1.setBounds(361, 211, 306, 2);
-        frmMetalplus.getContentPane().add(separator_1);
+     
         
         JLabel imagen = new JLabel("");
         imagen.setBounds(-35, 0, 358, 469);
@@ -87,7 +132,7 @@ public class Login {
         JLabel usuarioTitulo = new JLabel("USUARIO");
         usuarioTitulo.setFont(new Font("JetBrains Mono NL SemiBold", Font.PLAIN, 14));
         usuarioTitulo.setForeground(new Color(160, 82, 45));
-        usuarioTitulo.setBounds(361, 160, 134, 14);
+        usuarioTitulo.setBounds(361, 162, 134, 14);
         frmMetalplus.getContentPane().add(usuarioTitulo);
         
         JLabel passTitulo = new JLabel("PASSWORD");
@@ -109,7 +154,7 @@ public class Login {
         passTxt.setBorder(null);
         passTxt.setText("**********");
         passTxt.setForeground(Color.LIGHT_GRAY);
-        passTxt.setBounds(364, 262, 303, 27);
+        passTxt.setBounds(361, 266, 305, 27);
         frmMetalplus.getContentPane().add(passTxt);
         passTxt.addMouseListener(new MouseAdapter() {
             @Override
@@ -132,9 +177,23 @@ public class Login {
                 passTxt.setText("*******");
             }
         });
-        userTxt.setBounds(361, 179, 306, 27);
+        userTxt.setBounds(361, 181, 302, 27);
         frmMetalplus.getContentPane().add(userTxt);
         userTxt.setColumns(10);
+        
+        JLabel lblNewLabel = new JLabel("MENU PRINCIPAL");
+        lblNewLabel.setForeground(Color.GRAY);
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        lblNewLabel.setFont(new Font("JetBrains Mono NL", Font.PLAIN, 13));
+        lblNewLabel.setBounds(390, 403, 249, 14);
+        frmMetalplus.getContentPane().add(lblNewLabel);
+        
+        JSeparator separator_1_1_1 = new JSeparator();
+        separator_1_1_1.setOpaque(true);
+        separator_1_1_1.setIgnoreRepaint(true);
+        separator_1_1_1.setBounds(358, 211, 306, 2);
+        frmMetalplus.getContentPane().add(separator_1_1_1);
         frmMetalplus.setBounds(100, 100, 725, 508);
         frmMetalplus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
