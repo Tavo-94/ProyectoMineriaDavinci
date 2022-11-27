458f001d0e41e3ad04a04b644d5f2eb460b0403e
nobuwobu <carolina97ho@gmail.com>
2022-11-27 19:45:26 -0300

    Squashed commit of the following:
    commit 1b3637b46aae4b43f6d2a88098157eaa87e3c722
    Merge: 9d96be3 402c8bf
    Author: nobuwobu <carolina97ho@gmail.com>
    Date:   Sun Nov 27 19:43:47 2022 -0300
    
        Merge branch 'prueba' of https://github.com/Tavo-94/ProyectoMineriaDavinci into prueba
    
    commit 402c8bfaf43a7a3ac56dcd4cf7a7f52e4284c731
    Author: OrtigosaTomas <ortigosatomas@gmail.com>
    Date:   Sun Nov 27 18:59:17 2022 -0300
    
        corrección de import accidental
    
    commit c7e67b8ea443321f6f4b84d86e4f3b3eea116f5f
    Author: OrtigosaTomas <ortigosatomas@gmail.com>
    Date:   Sun Nov 27 18:57:49 2022 -0300
    
        Validaciones
    
    commit 9a0b23405f4efb82831c6f4c34ba5796111da783
    Merge: 9139d16 7bb81dc
    Author: Denise-juli <112222707+Denise-juli@users.noreply.github.com>
    Date:   Sun Nov 27 15:36:03 2022 -0300
    
        Merge branch 'prueba' of https://github.com/Tavo-94/ProyectoMineriaDavinci into prueba
    
    commit 9139d16d2b5c57a37a94c1b87048484cbcaf321f
    Author: Denise-juli <112222707+Denise-juli@users.noreply.github.com>
    Date:   Sun Nov 27 15:35:47 2022 -0300
    
        cambios para mostrar tabla y testeo

--- proyectoMineria/src/proyectoMineria/Main.java
+++ proyectoMineria/src/proyectoMineria/Main.java
@@ -1,77 +1,16 @@
 package proyectoMineria;
 
-import java.util.Scanner;
-
-import javax.swing.JOptionPane;
-
 import interfazWindowBuilder.PantallaPrincipal;
-<<<<<<< HEAD
-import interfazWindowBuilder.mainpantalla;
-=======
->>>>>>> prueba
-import negocio.AdminVentasDao;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-        mainpantalla pantalla = new mainpantalla();
-        pantalla.run();
-        System.out.println("pantalla principal");
-        
-        //test conexion
-        Mineria metalPlus = new Mineria("Metal plus");
-
-        AdminSistema gus = new AdminSistema("Gus94", "456", "Admin", true, metalPlus);
-        
-        AdminVentas dani = new AdminVentas("daniBoy78", "1234", "Ventas", true, metalPlus);
-        
-        AdminVentasDao ventasDAO = new AdminVentasDao();
-        
-        //tests del AdminSistema
-        
-        // Funcional
-        //gus.crearUsuario();
-        
-        // Funcional        
-        //gus.eliminarAdminVentas();
-        
-        // Funcional
-        //gus.eliminarAdminStock();
-        
-        
-        
-        //tests del AdminVentas
-        
-        //Es funcional
-        //dani.nuevoTicket();
-        
-        //Es funcional
-        //dani.visualizarStock();
-        
-        //Es funcional
-        //dani.visualizarOperacionesDeVenta();
-        
-        //test Validacion del LogIn ventas
-        
-          if (ventasDAO.validarLoginVentas(dani)) {
-          JOptionPane.showMessageDialog(null, "Login Exitoso", "Validacion LogIn",
-          JOptionPane.INFORMATION_MESSAGE);
-          } else {
-          JOptionPane.showMessageDialog(null, "Error, volver a intentarlo",
-          "Validacion LogIn", JOptionPane.WARNING_MESSAGE);
-          }
-         
-                
-        
-=======
     	
     	PantallaPrincipal app = new PantallaPrincipal();
     	app.Aplicacion();
        
->>>>>>> prueba
-
     }
 
 }
