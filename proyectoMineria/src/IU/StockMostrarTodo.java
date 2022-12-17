bf0dfb43617b978d0e399d9ce9ce9b84325b48a6
Tavo-94 <gustavo.mph@gmail.com>
2022-12-17 12:57:41 -0300

    c

---
+++ proyectoMineria/src/IU/StockMostrarTodo.java
@@ -0,0 +1,70 @@
+package IU;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import negocio.MaterialDAO;
+
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableModel;
+
+public class StockMostrarTodo extends JFrame {
+
+	private JPanel contentPane;
+	private JScrollPane scrollPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StockMostrarTodo ventanaStock = new StockMostrarTodo();
+					ventanaStock.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StockMostrarTodo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 822, 606);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		MaterialDAO matDAO = new MaterialDAO();
+
+		table = new JTable();
+		table.setModel(matDAO.mostrarTodoElStockDelDepo());
+		
+		
+		
+
+		
+		
+		
+		
+	}
+}
--- proyectoMineria/src/proyectoMineria/Main.java
+++ proyectoMineria/src/proyectoMineria/Main.java
@@ -1,6 +1,7 @@
 package proyectoMineria;
 
 import interfazWindowBuilder.PantallaPrincipal;
+import negocio.ClienteDAO;
 
 
 
@@ -11,6 +12,16 @@ public class Main {
     	PantallaPrincipal app = new PantallaPrincipal();
     	app.Aplicacion();
        
+    	//ClienteDAO clientedao = new ClienteDAO();
+    	//Material test1 = new Material("oro", "alta", 10d);
+    	
+    	//System.out.println(test1);
+    	
+    	
+    	//System.out.println(clientedao.obtenerDatosPorId(2));
+    	//System.out.println("hola");
+    	
+    	
     }
 
 }
